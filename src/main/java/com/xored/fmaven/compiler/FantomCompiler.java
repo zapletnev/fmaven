42257f5aa037323a2780f3421074e043c53d5457
Igor Zapletnev <igor.zapletnev@gmail.com>
2013-05-09 17:14:50 +0700

    Update fmavenfan jar file + repo

--- src/main/java/com/xored/fmaven/compiler/FantomCompiler.java
+++ src/main/java/com/xored/fmaven/compiler/FantomCompiler.java
@@ -1,41 +1,37 @@
 package com.xored.fmaven.compiler;
 
-import static com.xored.fmaven.compiler.CompileStatus.ERROR;
-import static com.xored.fmaven.compiler.CompileStatus.SUCCESSFUL;
 import static com.xored.fmaven.utils.PathUtils.platformPath;
 
 import java.io.File;
 
 import com.google.common.base.Joiner;
 
-import fan.fmaven.Compiler;
+import fan.fmaven.FCompiler;
 import fan.fmaven.FanPod;
 import fan.sys.List;
 import fan.sys.Uri;
 
 public class FantomCompiler {
 
-	@SuppressWarnings("static-access")
 	public CompileStatus compile(FanPod pod, File fanOutputDir, File podRepo) {
 		if (!fanOutputDir.exists()) {
 			fanOutputDir.mkdirs();
 		}
-		Compiler compiler = new Compiler().make(
-				Uri.fromStr(platformPath(fanOutputDir, true)),
-				Uri.fromStr(platformPath(podRepo, true)));
-		compiler.pods.add(pod);
-
-		List errors = compiler.compilePods();
+		FCompiler compiler = FCompiler.make(path(fanOutputDir),
+				path(podRepo));
+		List errors = compiler.compile(pod);
 		compiler.dispose();
 
 		if (errors.isEmpty()) {
-			return status(SUCCESSFUL);
-		} else {
-			status(ERROR, Joiner.on("; ").join(errors.toArray()));
-		}
-		return status(SUCCESSFUL);
+			return status(CompileStatus.SUCCESSFUL);
+		} 
+		return status(CompileStatus.ERROR, Joiner.on("; ").join(errors.toArray()));
 	}
 
+	private static Uri path(File f) {
+		return Uri.fromStr(platformPath(f, true));
+	}
+	
 	private static CompileStatus status(int code, String msg) {
 		return new CompileStatus(code, msg);
 	}
